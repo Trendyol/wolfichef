89c9bdf820f997427ea965241bd3fafeac63c6c9
Ömer Kaan Adaş <adaskaan@gmail.com>
2024-05-09 14:22:21 +0300

    Create secrets-test.go
    for testing purposes

---
+++ configs/secrets-test.go
@@ -0,0 +1,21 @@
+package configs
+
+// Secrets struct holds the secret values
+type Secrets struct {
+	Username        string
+	Password        string
+	slack_api_token string
+	AccessToken     string
+	APIKey          string
+}
+slack_api_token = "dummy_api_key"
+// GetSecrets returns the Secrets struct with secret values
+func GetSecrets() Secrets {
+	return Secrets{
+		Username:        "dummy_username",
+		Password:        "dummy_password",
+		slack_api_token: "dummy_api_key",
+		AccessToken:     "dummy_access_token",
+		APIKey:          "dummy_api_key",
+	}
+}
